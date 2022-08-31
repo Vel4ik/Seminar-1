a1c8927af3b10e06f0d082edaad4f10b27d83f0f
Valentina <vkpavlenina@mail.ru>
2022-08-31 19:56:26 +0500

    Task_2 project

---
+++ Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        if (number1 > number2)
+        {
+            Console.Write("max = ");
+            Console.WriteLine(number1);
+        }
+        else
+        {
+            Console.Write("max = ");
+            Console.WriteLine(number2);
+        }
+    }
+}
