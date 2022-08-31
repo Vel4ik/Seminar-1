30161ec87d020ad9f8fe67cb0e2468e3958b08e1
Valentina <vkpavlenina@mail.ru>
2022-08-31 21:36:40 +0500

    Task_8 project

---
+++ Task_8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int index = 0;
+        for (int i = 1; i <= number; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
