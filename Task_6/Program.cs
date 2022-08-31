fc8f11dd6498b0d1ab7fefc498b8d28c46d9d018
Valentina <vkpavlenina@mail.ru>
2022-08-31 21:02:54 +0500

    Task_6 project

---
+++ Task_6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 2 == 1)
+        {
+            Console.WriteLine("Число нечётное");
+        }
+        else
+        {
+            Console.WriteLine("Число чётное");
+        }
+    }
+}
